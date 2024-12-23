ec96607dcbe5e290de39b34147421abb9f04ac6e
Gazizov Niyaz <niaz@mail.ru>
2024-12-23 21:42:46 +0500

    Немного исправил решение

--- Database.php
+++ Database.php
@@ -14,86 +14,78 @@ class Database implements DatabaseInterface
         $this->mysqli = $mysqli;
     }
 
+    /**
+     * @throws Exception
+     */
     public function buildQuery(string $query, array $args = []): string
     {
-        try {
-            $i = -1;
-            $query_string = preg_replace_callback(
-                '/(\?)[d|f|a|#|$]|(\?)/x',
-                function ($matches) use (&$i, $args) {
-                    ++$i;
-                    $str = match ($matches[0]) {
-                        '?#' => match (true) {
-                            is_null($args[$i]) => 'NULL',
-                            $this->skip() === $args[$i] => $args[$i],
-                            is_string($args[$i]) => "`{$args[$i]}`",
-                            is_array($args[$i]) => implode(", ", $args[$i] = array_map(fn($a) => "`{$a}`", $args[$i])),
-                            default => throw new Exception(),
-                        },
-                        '?a' => match (true) {
-                            is_null($args[$i]) => 'NULL',
-                            $this->skip() === $args[$i] => $args[$i],
-                            is_array($args[$i]) => (function () use ($i, $args) {
-                                if (array_is_list($args[$i])) {
-                                    return implode(", ", $args[$i]);
-                                } else {
-                                    $arraySet = array_map(function ($k, $v): string {
+        $i = -1;
+        $query_string = preg_replace_callback(
+            '/(\?)[d|f|a|#|$]|(\?)/x',
+            function ($matches) use (&$i, $args) {
+                ++$i;
+                $str = match ($matches[0]) {
+                    '?#' => $this->checkNullAndSkip($args[$i]) ?? match (true) {
+                        is_string($args[$i]) => "`{$args[$i]}`",
+                        is_array($args[$i]) => implode(
+                            ", ",
+                            array_map(fn($a) => "`{$a}`", $args[$i]),
+                        ),
+                        default => throw new Exception(),
+                    },
+                    '?a' => $this->checkNullAndSkip($args[$i]) ?? match (true) {
+                        is_array($args[$i]) => (function () use ($i, $args) {
+                            if (array_is_list($args[$i])) {
+                                return implode(", ", $args[$i]);
+                            } else {
+                                $arraySet = array_map(
+                                    function ($k, $v): string {
                                         $b = $this->formatValue($v);
 
-                                        return "`$k` = ".(is_string($b) ? addslashes($b) : $b);
-                                    }, array_keys($args[$i]), array_values($args[$i]));
+                                        return "`$k` = " . (is_string($b)
+                                                ? addslashes($b) : $b);
+                                    },
+                                    array_keys($args[$i]),
+                                    array_values($args[$i]),
+                                );
 
-                                    return implode(", ", $arraySet);
-                                }
-                            })(),
-                            default => throw new Exception(),
-                        },
-                        '?d' => match (true) {
-                            is_null($args[$i]) => 'NULL',
-                            $this->skip() === $args[$i] => $args[$i],
-                            default => intval($args[$i])
-                        },
-                        '?f' => match (true) {
-                            is_null($args[$i]) => 'NULL',
-                            $this->skip() === $args[$i] => $args[$i],
-                            default => floatval($args[$i])
-                        },
-                        '?' => match (true) {
-                            is_null($args[$i]) => 'NULL',
-                            $this->skip() === $args[$i] => $args[$i],
-                            is_string($args[$i]) => addslashes("'{$args[$i]}'"),
-                            default => $this->formatValue($args[$i])
-                        },
-                        default => throw new Exception(),
-                    };
+                                return implode(", ", $arraySet);
+                            }
+                        })(),
+                        default => throw new Exception('Ошибка спецификатора'),
+                    },
+                    '?d' => $this->checkNullAndSkip($args[$i]) ?? intval($args[$i]),
+                    '?f' => $this->checkNullAndSkip($args[$i]) ?? floatval($args[$i]),
+                    '?' => $this->checkNullAndSkip($args[$i]) ?? match (true) {
+                        is_string($args[$i]) => addslashes("'{$args[$i]}'"),
+                        default => $this->formatValue($args[$i])
+                    },
+                    default => throw new Exception('Неправильный идентификатор'),
+                };
 
-                    return $str;
-                },
-                $query
-            );
+                return $str;
+            },
+            $query,
+        );
 
-            $regex_skip = "/[\{](.+)?({$this->skip()}){1}(.+)?[\}$]/";
-            return preg_replace([$regex_skip, '[{|}]'], [''], $query_string);
-
-        } catch (Exception $e) {
-            echo $e->getMessage();
-            throw new Exception();
-        }
+        $regex_skip = "/[\{](.+)?({$this->skipValue()}){1}(.+)?[\}$]/";
+        return preg_replace([$regex_skip, '[{|}]'], [''], $query_string);
     }
 
     /**
-     * Специальное занчение, присутствие которого внутри блока { специальное_значение }
+     * Специальное значение, размещенный внутри блока {специальное_значение}
      * запроса означает, что данный блок должен быть удален с данного запроса
+     *
      * @return string
      */
-    public function skip(): string
+    public function skipValue(): string
     {
         return 'skip_for_block';
     }
 
     /**
-     * Проверяем типы входищего параметра и форматируем
-     * в зависимости от типа
+     * Проверяем типы входящего параметра и
+     * форматируем в зависимости от типа
      *
      * @param mixed $value
      *
@@ -102,15 +94,27 @@ class Database implements DatabaseInterface
      */
     private function formatValue(mixed $value): float|int|string|Exception
     {
-        $result = match (true) {
+        return match (true) {
             is_bool($value) => true ? 1 : 0,
             is_string($value) => "'$value'",
-            is_int($value) => $value,
+            is_int($value), is_float($value) => $value,
             is_null($value) => 'NULL',
-            is_float($value) => $value,
-            default => throw new Exception(),
+            default => throw new Exception('Ошибка типа'),
         };
+    }
 
-        return $result;
+    /**
+     * Проверка на null и специальное значение
+     * @param mixed $value
+     *
+     * @return mixed
+     */
+    private function checkNullAndSkip(mixed $value): mixed
+    {
+        return match (true) {
+            is_null($value) => 'NULL',
+            $this->skipValue() === $value => $value,
+            default => null
+        };
     }
 }
--- test.php
+++ test.php
@@ -3,22 +3,34 @@
 use FpDbTest\Database;
 use FpDbTest\DatabaseTest;
 
-spl_autoload_register(function ($class) {
-    $a = array_slice(explode('\\', $class), 1);
-    if (!$a) {
-        throw new Exception();
-    }
-    $filename = implode('/', [__DIR__, ...$a]) . '.php';
-    require_once $filename;
-});
+try {
+    spl_autoload_register(
+        function ($class) {
+            $a = array_slice(explode('\\', $class), 1);
+            if (!$a) {
+                throw new Exception('Ошибка подключения класса');
+            }
+            $filename = implode('/', [__DIR__, ...$a]) . '.php';
+            require_once $filename;
+        },
+    );
 
-$mysqli = @new mysqli('db', 'root', 'password', 'database', 3306);
-if ($mysqli->connect_errno) {
-    throw new Exception($mysqli->connect_error);
+    $mysqli = @new mysqli('db', 'root', 'password', 'database', 3306);
+    if ($mysqli->connect_errno) {
+        throw new Exception($mysqli->connect_error);
+    }
+} catch (Exception $exception) {
+    echo $exception->getMessage();
+    die();
 }
 
-$db = new Database($mysqli);
-$test = new DatabaseTest($db);
-$test->testBuildQuery();
+try {
+    $db = new Database($mysqli);
+    $test = new DatabaseTest($db);
+    $test->testBuildQuery();
+} catch (Exception $exception) {
+    echo $exception->getMessage();
+    die();
+}
 
 exit("OK\n");
